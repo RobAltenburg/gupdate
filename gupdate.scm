f533db5105f4076ba642c617f849619d1bdc9b57
Robert Altenburg <rca@qrpc.com>
2017-01-10 14:28:01 -0500

    added command-line entry

--- gupdate.scm
+++ gupdate.scm
@@ -1,24 +1,24 @@
 (use srfi-1 fmt posix)
 
 (define (update-directory thunk dir)
-  (if (null? dir)
-    #t
-    (let ((old-dir (current-directory))
-          (proc-dir (car dir)))
-      (change-directory proc-dir)
+  (unless (null? dir)
+      (change-directory (car dir))
       (thunk)
-      (update-directory thunk (filter directory? (directory (current-directory))))
-      (change-directory old-dir)
-      (update-directory thunk (cdr dir)))))
-
+      (update-directory thunk (append (cdr dir)
+          (map (lambda (x) 
+                 (string-append (current-directory) "/" x)) 
+                    (filter directory? (directory (current-directory))))))))
+      
 (define (git-update-dir)
     (when (file-exists? ".git")
       (fmt #t "In: " (current-directory) nl)  
       (system "git pull")
       (fmt #t nl)))
 
-(define start-directory "~/.vim/bundle")
-(change-directory start-directory)
-(update-directory git-update-dir (directory start-directory))
+(let ((dirlist (command-line-arguments)))
+  (if (null? dirlist)
+  (fmt #t "Usage:  gupdate <directory>" nl 
+       "Runs \"git pull\" in the directory and any git repositories in subdirectories." nl)
+    (update-directory git-update-dir dirlist)))
                   
 
