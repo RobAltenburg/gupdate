3a2d74845ecafd106ac561b8a159f26555964940
Robert Altenburg <rca@qrpc.com>
2017-01-10 13:23:25 -0500

    ~

--- gupdate.scm
+++ gupdate.scm
@@ -1,29 +1,24 @@
-(use posix)
+(use srfi-1 fmt posix)
+
+(define (update-directory thunk dir)
+  (if (null? dir)
+    #t
+    (let ((old-dir (current-directory))
+          (proc-dir (car dir)))
+      (change-directory proc-dir)
+      (thunk)
+      (update-directory thunk (filter directory? (directory (current-directory))))
+      (change-directory old-dir)
+      (update-directory thunk (cdr dir)))))
+
+(define (git-update-dir)
+    (when (file-exists? ".git")
+      (fmt #t "In: " (current-directory) nl)  
+      (system "git pull")
+      (fmt #t nl)))
 
 (define start-directory "/Users/rca/.vim/bundle")
-
-(define process-subdirectory
-  (lambda (d)
-         (if (directory? d)
-           (begin
-             (change-directory d)
-             (format #t "~%In dir: ~A..." (current-directory))
-             (if (file-exists? ".git")
-                (system "git pull")
-                (write "no go"))
-             (change-directory "..")
-             )
-           '())))
-
-(define process-directories 
-  (lambda (d)
-     (if (null? d)
-        (write "done")
-        (let ([dw (car d)])
-           (process-subdirectory dw)
-           (process-directories (cdr d)))
-        )))
-
 (change-directory start-directory)
-(process-directories (directory start-directory))
+(update-directory git-update-dir (directory start-directory))
+                  
 
