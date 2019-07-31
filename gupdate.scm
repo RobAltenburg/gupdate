1f1c0185e141198b866c9003f491e5e09410295f
Robert Altenburg <rca@qrpc.com>
2019-07-31 19:32:56 -0400

    reimplemented for Chicken5

--- gupdate.scm
+++ gupdate.scm
@@ -1,7 +1,8 @@
 ;;;; gupdate
 ; 
 ; a program to run "git pull" in a series of subdirectories
-;
+; Right now it does not descend recursively to nested subdirectories
+; but the "levels" variable is there to make that an option
 ;
 
 (import srfi-1
@@ -12,21 +13,25 @@
         (chicken format)
         (chicken file posix))
 
+(define (full-directory #!optional (pathname (current-directory)))
+    (map (lambda (dd)
+           (string-append pathname "/" dd)) (directory pathname)))
+
 (define (process-directory start-dir in-dir levels) 
   (change-directory in-dir)
-  (printf "in directory ~A~%" in-dir) 
+  (printf "Checking ~A~%" in-dir) 
   (when (file-exists? ".git")
-    (print "Found git")
     (system "git pull")
     )
   (change-directory start-dir))
 
 (define (select-directories #!optional (start-directory (current-directory)) (number-of-levels 1))
-  (print "Running in " start-directory)
-  (print "Levels: " number-of-levels)
-  (let ((sub-directories (filter directory? (directory start-directory))))
+  (let ((sub-directories (filter directory? (full-directory start-directory))))
     (map (lambda (dd)
            (process-directory start-directory dd number-of-levels)) sub-directories)))
 
-(select-directories)
+(let ((args (command-line-arguments)))
+  (if (null? args)
+      (select-directories)
+      (select-directories (car (reverse args)) 1)))
 
